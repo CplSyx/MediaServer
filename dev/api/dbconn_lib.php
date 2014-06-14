b6e1d4c16398e2a52fa14940cf1067910a481f7a
Sam <cplsyx@gmail.com>
2014-06-14 19:34:49 +0100

    Updated to display more information from DB

--- dev/api/dbconn_lib.php
+++ dev/api/dbconn_lib.php
@@ -4,5 +4,10 @@ function getConnectionString($database)
     if($database == "MediaServer")
         return array("Database"=>"MediaServer", "UID"=>"webuser", "PWD"=>"f1shs0up");
 }
+function getSQLServerName()
+{
+    return "(local)";
+}
+
 
 ?>
